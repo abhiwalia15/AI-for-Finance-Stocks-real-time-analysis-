b9d915757211afc2736c916f3c3a79a441b83efa
abhiwalia15 <waliamrinal@gmail.com>
2020-05-05 03:39:13 +0530

    all imports files

---
+++ RealTime/imports_and_read.py
@@ -0,0 +1,13 @@
+#Import the libraries
+import math
+import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+from nsepy import get_history
+from datetime import date
