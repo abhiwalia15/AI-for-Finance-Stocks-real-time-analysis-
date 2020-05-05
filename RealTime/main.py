a2b6887397e06332bfa01235eeef7cfecd414952
abhiwalia15 <waliamrinal@gmail.com>
2020-05-05 07:03:46 +0530

    cache files

--- RealTime/main.py
+++ RealTime/main.py
@@ -1,14 +1,37 @@
 from imports_and_read import *
 
-st.title("NSE Real-Time Stocks Analysis")
-st.header("Enter the symbol of the stock you want to analyse and we will predict the next day Close Price for you")
+def main():
 
-#symbol = input("Enter symbol of stock\n")
-symbol = st.text_input("Enter symbol")
+    st.title("NSE Real-Time Stocks Analysis")
+    st.header("Select the stock and check its next day predicted value")
 
-df = get_history(symbol=symbol, start=date(2010,1,1), end=date.today())
-df['Date'] = df.index
+    # Stock Section
+    choose_stock = st.sidebar.selectbox("Choose the Stock ---->>",
+        ["NONE","Aditya Birla Fashion Retail Ltd.", "PowerMech Solns."])
 
-st.dataframe(df)
+    if(choose_stock == "Aditya Birla Fashion Retail Ltd."):
 
+        # get abfrl real time stock price
+        df1 = get_history(symbol='abfrl', start=date(2010,1,1), end=date.today())
+        df1['Date'] = df.index
+        
+        # Insert Check-Box to show the snippet of the data.
+        if st.checkbox('Show Raw Data'):
+            st.subheader("Showing raw data---->>>")	
+            st.write(df1.tail())
+        
 
+    elif(choose_stock == "PowerMech Solns."):
+
+        # get abfrl real time stock price
+        df2 = get_history(symbol='powermech', start=date(2010,1,1), end=date.today())
+        df2['Date'] = df.index
+        
+        # Insert Check-Box to show the snippet of the data.
+        if st.checkbox('Show Raw Data'):
+            st.subheader("Showing raw data---->>>")	
+            st.write(df2.tail())
+
+if __name__ == '__main__':
+
+    main()
