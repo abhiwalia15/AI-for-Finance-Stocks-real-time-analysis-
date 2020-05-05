6baf98b0132d74403c8f19994799bbf4154f629e
abhiwalia15 <waliamrinal@gmail.com>
2020-05-05 09:36:09 +0530

    commit done

--- RealTime/imports_and_read.py
+++ RealTime/imports_and_read.py
@@ -8,8 +8,32 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
+from keras.models import load_model
+from nsepy import get_history
+from datetime import date
+import datetime
+import streamlit as st
+
+#Import the libraries
+import math
+import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 
 from nsepy import get_history
 from datetime import date
 
-import streamlit as st
+import plotly.offline as pyo
+import plotly.graph_objs as go
+from plotly.offline import iplot
+import altair as alt
+import cufflinks as cf
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot 
+import plotly.express as px
+
+cf.go_offline()
--- RealTime/main.py
+++ RealTime/main.py
@@ -1,4 +1,4 @@
-from imports_and_read import *
+from imports_and_read import * 
 
 def main():
 
@@ -6,31 +6,107 @@ def main():
     st.header("Select the stock and check its next day predicted value")
 
     # Stock Section
-    choose_stock = st.sidebar.selectbox("Choose the Stock ---->>",
+    choose_stock = st.sidebar.selectbox("Choose the Stock!",
         ["NONE","Aditya Birla Fashion Retail Ltd.", "PowerMech Solns."])
 
     if(choose_stock == "Aditya Birla Fashion Retail Ltd."):
 
         # get abfrl real time stock price
         df1 = get_history(symbol='abfrl', start=date(2010,1,1), end=date.today())
-        df1['Date'] = df.index
+        df1['Date'] = df1.index
+
+        st.header("Aditya Birla Fashion NSE Last 5 Days DataFrame:")
         
         # Insert Check-Box to show the snippet of the data.
         if st.checkbox('Show Raw Data'):
             st.subheader("Showing raw data---->>>")	
-            st.write(df1.tail())
+            st.dataframe(df1.tail())
         
+        ## Predictions and adding it to Dashboard
+        #Create a new dataframe
+        new_df = df1.filter(['Close'])
+        #Scale the all of the data to be values between 0 and 1 
+        scaler = MinMaxScaler(feature_range=(0, 1)) 
+        scaled_data = scaler.fit_transform(new_df)
+        #Get teh last 30 day closing price 
+        last_30_days = new_df[-30:].values
+        #Scale the data to be values between 0 and 1
+        last_30_days_scaled = scaler.transform(last_30_days)
+        #Create an empty list
+        X_test = []
+        #Append teh past 1 days
+        X_test.append(last_30_days_scaled)
+        #Convert the X_test data set to a numpy array
+        X_test = np.array(X_test)
+        #Reshape the data
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        #load the model
+        model = load_model("abfrl.model")
+        #Get the predicted scaled price
+        pred_price = model.predict(X_test)
+        #undo the scaling 
+        pred_price = scaler.inverse_transform(pred_price)
+        
+        # next day
+        NextDay_Date = datetime.date.today() + datetime.timedelta(days=1)
+
+        st.subheader("Predictions for the next upcoming day : " + str(NextDay_Date))
+        st.markdown(pred_price)
+
+        ##visualizations
+        st.subheader("Close Price VS Date Interactive chart for analysis : ")
+        
+        st.area_chart(df1['Close'])
 
     elif(choose_stock == "PowerMech Solns."):
 
         # get abfrl real time stock price
         df2 = get_history(symbol='powermech', start=date(2010,1,1), end=date.today())
-        df2['Date'] = df.index
+        df2['Date'] = df2.index
         
+        st.header("PowerMech Soln. NSE Last 5 Days DataFrame:")
+
         # Insert Check-Box to show the snippet of the data.
         if st.checkbox('Show Raw Data'):
             st.subheader("Showing raw data---->>>")	
-            st.write(df2.tail())
+            st.dataframe(df2.tail())
+
+        ## Predictions and adding it to Dashboard
+        #Create a new dataframe
+        new_df = df2.filter(['Close'])
+        #Scale the all of the data to be values between 0 and 1 
+        scaler = MinMaxScaler(feature_range=(0, 1)) 
+        scaled_data = scaler.fit_transform(new_df)
+        #Get teh last 30 day closing price 
+        last_30_days = new_df[-30:].values
+        #Scale the data to be values between 0 and 1
+        last_30_days_scaled = scaler.transform(last_30_days)
+        #Create an empty list
+        X_test = []
+        #Append teh past 1 days
+        X_test.append(last_30_days_scaled)
+        #Convert the X_test data set to a numpy array
+        X_test = np.array(X_test)
+        #Reshape the data
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        #load the model
+        model = load_model("powermech.model")
+        #Get the predicted scaled price
+        pred_price = model.predict(X_test)
+        #undo the scaling 
+        pred_price = scaler.inverse_transform(pred_price)
+        
+        # next day
+        NextDay_Date = datetime.date.today() + datetime.timedelta(days=1)
+
+        st.subheader("Predictions for the next upcoming day : " + str(NextDay_Date))
+        st.markdown(pred_price)
+
+        ##visualizations
+        st.subheader("Close Price VS Date Interactive chart for analysis : ")
+        
+        st.area_chart(df2['Close'])
+
 
 if __name__ == '__main__':
 
