c94b55b88c4cf80ad450add307208c018756aeda
abhiwalia15 <waliamrinal@gmail.com>
2020-05-05 06:41:50 +0530

    final future stocks pred

--- RealTime/main.py
+++ RealTime/main.py
@@ -1,11 +1,14 @@
 from imports_and_read import *
 
-symbol = input("Enter symbol of stock\n")
-st.write(symbol)
+st.title("NSE Real-Time Stocks Analysis")
+st.header("Enter the symbol of the stock you want to analyse and we will predict the next day Close Price for you")
+
+#symbol = input("Enter symbol of stock\n")
+symbol = st.text_input("Enter symbol")
 
 df = get_history(symbol=symbol, start=date(2010,1,1), end=date.today())
 df['Date'] = df.index
 
-
+st.dataframe(df)
 
 
