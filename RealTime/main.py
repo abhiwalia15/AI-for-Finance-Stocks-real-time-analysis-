841b3b8a550571081892da848bcb372d6834db6e
abhiwalia15 <waliamrinal@gmail.com>
2020-05-06 13:55:59 +0530

    inox stocks model

--- RealTime/main.py
+++ RealTime/main.py
@@ -55,8 +55,16 @@ def main():
 
         ##visualizations
         st.subheader("Close Price VS Date Interactive chart for analysis : ")
-        
         st.area_chart(df1['Close'])
+
+        st.subheader("Open Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['Open'])
+
+        st.subheader("High Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['High'])
+
+        st.subheader("Low Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['Low'])
 
     elif(choose_stock == "PowerMech Solns."):
 
@@ -106,6 +114,64 @@ def main():
         st.subheader("Close Price VS Date Interactive chart for analysis : ")
         
         st.area_chart(df2['Close'])
+
+    
+    if(choose_stock == "Aditya Birla Fashion Retail Ltd."):
+
+        # get abfrl real time stock price
+        df1 = get_history(symbol='abfrl', start=date(2010,1,1), end=date.today())
+        df1['Date'] = df1.index
+
+        st.header("Aditya Birla Fashion NSE Last 5 Days DataFrame:")
+        
+        # Insert Check-Box to show the snippet of the data.
+        if st.checkbox('Show Raw Data'):
+            st.subheader("Showing raw data---->>>")	
+            st.dataframe(df1.tail())
+        
+        ## Predictions and adding it to Dashboard
+        #Create a new dataframe
+        new_df = df1.filter(['Close'])
+        #Scale the all of the data to be values between 0 and 1 
+        scaler = MinMaxScaler(feature_range=(0, 1)) 
+        scaled_data = scaler.fit_transform(new_df)
+        #Get teh last 30 day closing price 
+        last_30_days = new_df[-30:].values
+        #Scale the data to be values between 0 and 1
+        last_30_days_scaled = scaler.transform(last_30_days)
+        #Create an empty list
+        X_test = []
+        #Append teh past 1 days
+        X_test.append(last_30_days_scaled)
+        #Convert the X_test data set to a numpy array
+        X_test = np.array(X_test)
+        #Reshape the data
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        #load the model
+        model = load_model("abfrl.model")
+        #Get the predicted scaled price
+        pred_price = model.predict(X_test)
+        #undo the scaling 
+        pred_price = scaler.inverse_transform(pred_price)
+        
+        # next day
+        NextDay_Date = datetime.date.today() + datetime.timedelta(days=1)
+
+        st.subheader("Predictions for the next upcoming day : " + str(NextDay_Date))
+        st.markdown(pred_price)
+
+        ##visualizations
+        st.subheader("Close Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['Close'])
+
+        st.subheader("Open Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['Open'])
+
+        st.subheader("High Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['High'])
+
+        st.subheader("Low Price VS Date Interactive chart for analysis : ")
+        st.area_chart(df1['Low'])
 
 
 if __name__ == '__main__':
